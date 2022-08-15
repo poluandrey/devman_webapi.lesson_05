03e6eb9f918f0dd2dd965a4439dc3ae667495b2d
a_polumestny <anpolumestny@gmail.com>
2022-08-15 20:49:59 +0300

    wrote function for create table

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Callable, List
+
 from terminaltables import AsciiTable
 
 import settings
@@ -5,34 +7,32 @@ from head_hunter import api as hh_api
 from super_job import api as sj_api
 
 
-def main():
-    sj_table = [
-        ['language', 'vacations found', 'vacations processed', 'average salary'],
+def create_table(languages: List, func: Callable, title: str):
+    table = [
+        ['language',
+         'vacations found',
+         'vacations processed',
+         'average salary'],
     ]
-    for language in settings.PROGRAM_LANGUAGES:
-        sj_vacations_info = sj_api.retrieve_vacation_info_by_language(language=language)
-        info = sj_vacations_info[language]
-        sj_row = [language,
-                  info['vacancies_found'],
-                  info['vacancies_processed'],
-                  info['average_salary']]
-        sj_table.append(sj_row)
-    table = AsciiTable(sj_table, title='SuperJob')
-    print(table.table)
 
-    hh_table = [
-        ['language', 'vacations found', 'vacations processed', 'average salary'],
-    ]
-    for language in settings.PROGRAM_LANGUAGES:
-        sj_vacations_info = hh_api.retrieve_vacation_info_by_language(language=language)
-        info = sj_vacations_info[language]
-        sj_row = [language,
-                  info['vacancies_found'],
-                  info['vacancies_processed'],
-                  info['average_salary']]
-        hh_table.append(sj_row)
-    table = AsciiTable(hh_table, title='SuperJob')
-    print(table.table)
+    for language in languages:
+        vacations_info = func(language)
+        info = vacations_info[language]
+        row = [language,
+               info['vacancies_found'],
+               info['vacancies_processed'],
+               info['average_salary']]
+        table.append(row)
+    table = AsciiTable(table, title=title)
+    return table
+
+
+def main():
+    languages = settings.PROGRAM_LANGUAGES
+    sj_table = create_table(languages=languages, func=sj_api.retrieve_vacation_info_by_language, title='SuperJob')
+    print(sj_table.table)
+    hh_table = create_table(languages=languages, func=hh_api.retrieve_vacation_info_by_language, title='HeadHunter')
+    print(hh_table.table)
 
 
 if __name__ == '__main__':
